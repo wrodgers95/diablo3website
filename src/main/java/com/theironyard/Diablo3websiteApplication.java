3372bed63bd7a1db5dbffa77c65a397d956e9145
wrodgers95 <will.rodgers1995@gmail.com>
2016-10-07 12:01:03 -0400

    added @EnableOauth2Sso annotation

--- src/main/java/com/theironyard/Diablo3websiteApplication.java
+++ src/main/java/com/theironyard/Diablo3websiteApplication.java
@@ -2,8 +2,10 @@ package com.theironyard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class Diablo3websiteApplication {
 
 	public static void main(String[] args) {
