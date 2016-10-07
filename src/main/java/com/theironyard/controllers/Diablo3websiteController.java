df83a76d65222d3ec3ed0ee530bf8552bb529398
wrodgers95 <will.rodgers1995@gmail.com>
2016-10-07 12:36:21 -0400

    added cloud Oauth2 and Spring Security dependencies

--- src/main/java/com/theironyard/controllers/Diablo3websiteController.java
+++ src/main/java/com/theironyard/controllers/Diablo3websiteController.java
@@ -1,18 +1,19 @@
 package com.theironyard.controllers;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 
 /**
  * Created by Blake on 10/5/16.
  */
-@Controller
+@RestController
 @SpringBootApplication
-public class Diablo3websiteController {
+public class Diablo3websiteController extends WebSecurityConfigurerAdapter {
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public static String index (HttpSession session) {
