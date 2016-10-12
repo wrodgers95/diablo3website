86700eb90a5b39b8539b62bcbdf8ccd6bbf87fd1
wrodgers95 <will.rodgers1995@gmail.com>
2016-10-12 10:59:53 -0400

    @RestController -> @Controller

--- src/main/java/com/theironyard/controllers/Diablo3websiteController.java
+++ src/main/java/com/theironyard/controllers/Diablo3websiteController.java
@@ -4,15 +4,15 @@ import com.theironyard.entities.User;
 import com.theironyard.services.UserRepository;
 import com.theironyard.utilities.PasswordStorage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@RestController
+@Controller
 public class Diablo3websiteController {
 
     @Autowired
