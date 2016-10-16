6b01ddf0c2628e8770109576cedcd6c83dba10e8
wrodgers95 <will.rodgers1995@gmail.com>
2016-10-16 11:39:31 -0400

    honestly not sure what I just changed, but the data is coming back as Json now

--- src/main/java/com/theironyard/controllers/Diablo3websiteController.java
+++ src/main/java/com/theironyard/controllers/Diablo3websiteController.java
@@ -2,11 +2,9 @@ package com.theironyard.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.theironyard.entities.User;
 import com.theironyard.entities.Item;
 import com.theironyard.services.ItemRepository;
 import com.theironyard.services.UserRepository;
-import com.theironyard.utilities.PasswordStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,9 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.io.IOException;
 import java.util.ArrayList;
 
 @Controller
@@ -31,7 +27,6 @@ public class Diablo3websiteController {
     @Autowired
     ItemRepository items;
 
-    @CrossOrigin
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String index(HttpSession session, Model model) {
 
@@ -40,7 +35,7 @@ public class Diablo3websiteController {
 
         ArrayList<String> jsonArray = new ArrayList<>();
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
 
             String itemUri = "https://us.api.battle.net/wow/item/"+ itemInput +"?locale=en_US&apikey=yz98b2qzp8qfp62axbgrmzsuzjkwbgc8";
 
@@ -48,46 +43,39 @@ public class Diablo3websiteController {
             Item itemJson = null;
             try {
                 itemJson = restTemplate.getForObject(itemUri, Item.class);
-                try     {items.save(itemJson); }
-                catch   (NullPointerException ex) {
-//                    System.out.println("No Items Saved");
-                }
-
+                try {items.save(itemJson);
+                } catch (NullPointerException ex) { }
             } catch (HttpClientErrorException ex) { }
-            // do not have to write back to a String, but rather save object to a repository
             try {
                 jsonData = new ObjectMapper().writeValueAsString(itemJson);
             } catch (JsonProcessingException ex) {
                 ex.printStackTrace();
             }
             jsonArray.add(jsonData);
-//          System.out.println(jsonData);
             itemInput++;
-            System.out.println(items);
         }
-        model.addAttribute("jsonArray",  jsonArray);
+        model.addAttribute("jsonArray", jsonArray);
         return "index";
     }
 
-    @RequestMapping(path = "/login", method = RequestMethod.POST)
-    public User login(String username, String password, HttpSession session, HttpServletResponse response) throws Exception {
-        User user = users.findFirstByName(username);
-        if (user == null) {
-            user = new User(username, PasswordStorage.createHash(password));
-            users.save(user);
-        }
-        else if (!PasswordStorage.verifyPassword(password, user.getPassword())) {
-            throw new Exception("Wrong password");
-        }
-        session.setAttribute("username", username);
-        response.sendRedirect("/");
-        return user;
-    }
-
-    @RequestMapping(path = "/logout", method = RequestMethod.POST)
-    public void logout(HttpSession session, HttpServletResponse response) throws IOException {
-        session.invalidate();
-        response.sendRedirect("/");
-    }
-
+//    @RequestMapping(path = "/login", method = RequestMethod.POST)
+//    public User login(String username, String password, HttpSession session, HttpServletResponse response) throws Exception {
+//        User user = users.findFirstByName(username);
+//        if (user == null) {
+//            user = new User(username, PasswordStorage.createHash(password));
+//            users.save(user);
+//        }
+//        else if (!PasswordStorage.verifyPassword(password, user.getPassword())) {
+//            throw new Exception("Wrong password");
+//        }
+//        session.setAttribute("username", username);
+//        response.sendRedirect("/");
+//        return user;
+//    }
+//
+//    @RequestMapping(path = "/logout", method = RequestMethod.POST)
+//    public void logout(HttpSession session, HttpServletResponse response) throws IOException {
+//        session.invalidate();
+//        response.sendRedirect("/");
+//    }
 }
